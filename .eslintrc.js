e5ee811a126f308d02af5d2b7e92598213db3d0e
Abdul Rahim <alizada1353@android-47d7c64d23f5b9b5.home>
2020-06-29 18:57:28 +0200

    cleaned npm cahe and reinstalled dependencies and installed eslint, configure es lint for detecting potential problems

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+const { truncate } = require("lodash");
+
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+  extends: "eslint:recommended",
+  rules: {
+    "no-multiple-empty-lines": "warn",
+    "no-var": "error",
+    "prefer-const": "error",
+  },
+};
